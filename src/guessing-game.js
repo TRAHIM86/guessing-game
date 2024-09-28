5df6391dde4e074be8be4085082a84f464d01de3
TRAHIM86 <trahim1986@gmail.com>
2024-09-29 01:25:31 +0300

    complete task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minNumber = min;    //значение минНамбер = мин
+        this.maxNumber = max;    //значение махНамбер = мах
     }
 
     guess() {
-
+        this.average = Math.round((this.minNumber + this.maxNumber)/2);      //среднее = округл(мин+мах)/2
+        return this.average;                                                 //this method returns solution candidate (метод возвращает число-предположение для решения)
     }
 
     lower() {
-
+        this.maxNumber = this.average;      //если this.average > искомого числа, то махНамбер = this.average 
     }
 
     greater() {
-
+        this.minNumber = this.average;      //если this.average < искомого числа, то минНамбер = this.average 
     }
 }
 
